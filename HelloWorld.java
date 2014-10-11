6769fca3f85ad09868b751d2dcd86245679b2a57
davidaj3 <jon11049@byui.edu>
2014-10-11 13:50:10 -0600

    Fixed some stuff

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,12 +1,9 @@
 public class HelloWorld {
 
    public static void main(String[] args) {
-<<<<<<< HEAD
       System.out.println("Hello, Github!");
-=======
       System.out.println("Hello, Github world!");
       System.out.println("Testing...");
->>>>>>> e500c0a2b2e078136e6c9b1874131a4e71086563
    }
 
 }
